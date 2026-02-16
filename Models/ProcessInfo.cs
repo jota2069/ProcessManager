b4b75172f6831dff3dc71bd5ef50addee773ceec
jota2069 <mkat4274@gmail.com>
2026-02-16 19:41:42 +0300

    feat(Models):Добавил модель ProcessInfo

--- Models/ProcessInfo.cs
+++ Models/ProcessInfo.cs
@@ -1,6 +1,16 @@
+using System;
+using System.Diagnostics;
+
 namespace ProcessManager.Models;
 
 public class ProcessInfo
 {
-    
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public ProcessPriorityClass Priority { get; set; }
+    public long MemoryUsage { get; set; }
+    public int ThreadCount { get; set; }
+    public TimeSpan CpuTime { get; set; }
+
+    public string MemoryUsageMB => $"{MemoryUsage / 1024 / 1024} MB";
 }
